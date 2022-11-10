72cbcb08e13a54439e034b4687c40337916b8c30
Anderson Costa <andersonfelipedc@gmail.com>
2022-11-10 13:42:16 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,11 @@
 package application;
 
+import boardGame.Position;
+
 public class Program {
     public static void main(String[] args) {
-        
-        System.out.println("Hello world");
+
+        Position pos = new Position(3,5);
+        System.out.println(pos);
     }
 }
